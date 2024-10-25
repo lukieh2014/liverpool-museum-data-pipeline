88cd139c536436bc6d90e006c66773ea152ee2c8
Luke <lukieh2014@gmail.com>
2024-10-25 17:09:32 +0100

    changes to output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,8 @@ output "rds_instance_address" {
   description = "Public IP address of the RDS instance"
   value       = aws_db_instance.c14-luke-museum-db.endpoint
 }
+
+output "ec2_instance_address" {
+  description = "Public IP address of the EC2 instance"
+  value = aws_instance.c14-luke-museum-ec2.public_ip
+}
